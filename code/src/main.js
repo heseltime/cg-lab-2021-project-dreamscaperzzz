021c39459b24ac8ad1bbfe3f7481555b8e29604c
heseltime <jack.heseltine@gmail.com>
2021-06-22 09:20:38 +0200

    spot nachziehen

--- code/src/main.js
+++ code/src/main.js
@@ -4,7 +4,7 @@ var gl = null,
 
 //Camera
 var cameraAutostartEnabled = true;
-var animationSpeedupFactor = 1.0/20.0*3.0;
+var animationSpeedupFactor = 1.0/2.0*3.0;
 var camera = null;
 var cameraPos = vec3.create();
 var cameraCenter = vec3.create();
@@ -188,6 +188,27 @@ function createSceneGraph(gl, resources) {
   // add light to scenegraph
   root.append(light);
 
+  // new light == spot
+  let light2 = new LightNode();
+  light2.uniform = 'u_light2';
+  light2.angle = 120.0;
+  light2.ambient = [0, 0, 0, 1];
+  light2.diffuse = [1, 0, 0, 1];
+  light2.specular = [1, 0, 0, 1];
+  light2.position = [13, 12, 0];
+  light2.append(createLightSphere());
+  rotateLight2 = new TransformationSGNode(mat4.create(), [
+      light2
+  ]);
+  root.append(rotateLight2);
+
+  // animate light 2
+  light2Animation = new Animation(rotateLight2, [
+    { matrix: progress => mat4.rotateY(mat4.create(), mat4.translate(mat4.create(), mat4.create(), [0, 0, 0]), glm.deg2rad(-360 * progress)), duration: 3000 },
+  ],
+  true);
+  light2Animation.start();
+
   //single mask
   let maskSurface = new MaterialSGNode([
     new RenderSGNode(makeMask())
@@ -327,6 +348,8 @@ function render(timeInMilliseconds) {
     billboardAnimationsRunning = true;
   }
 
+  // light updates
+  light2Animation.update(deltaTime);
 
   // BILLBOARDING CONVERGE TEST
   // wait for circle animation to finish --> converge to camera position
@@ -835,4 +858,84 @@ function makeMask() {
     texture: texture,
     index: index
   };
+}
+
+// Classes
+
+// Custom LightNode to implement spot light, here and in phong fs
+class LightNode extends TransformationSGNode {
+
+  constructor(position, children) {
+    super(null, children);
+    this.position = position || [0, 0, 0];
+    this.ambient = [0, 0, 0, 1];
+    this.diffuse = [1, 1, 1, 1];
+    this.specular = [1, 1, 1, 1];
+    //uniform name
+    //this.uniform = 'u_light2';
+
+    this._worldPosition = null;
+
+    //spot
+    this.direction = [10, 100, 100];
+  }
+
+  setLightUniforms(context) {
+    const gl = context.gl;
+    //no materials in use
+    if (!context.shader || !isValidUniformLocation(gl.getUniformLocation(context.shader, this.uniform+'.ambient'))) {
+      return;
+    }
+    gl.uniform4fv(gl.getUniformLocation(context.shader, this.uniform+'.ambient'), this.ambient);
+    gl.uniform4fv(gl.getUniformLocation(context.shader, this.uniform+'.diffuse'), this.diffuse);
+    gl.uniform4fv(gl.getUniformLocation(context.shader, this.uniform+'.specular'), this.specular);
+
+    //spot
+    gl.uniform1f(gl.getUniformLocation(context.shader, this.uniform+'Angle'), this.angle);
+  }
+
+  setLightPosition(context) {
+    const gl = context.gl;
+    if (!context.shader || !isValidUniformLocation(gl.getUniformLocation(context.shader, this.uniform+'Pos'))) {
+      return;
+    }
+    const position = this._worldPosition || this.position;
+    gl.uniform3f(gl.getUniformLocation(context.shader, this.uniform+'Pos'), position[0], position[1], position[2]);
+
+    //spot
+    const direction = this.direction;
+    gl.uniform3f(gl.getUniformLocation(context.shader, this.uniform+'Direction'), direction[0], direction[1], direction[2]);
+  }
+
+  computeLightPosition(context) {
+    //transform with the current model view matrix
+    const modelViewMatrix = mat4.multiply(mat4.create(), context.viewMatrix, context.sceneMatrix);
+    const original = this.position;
+    const position =  vec4.transformMat4(vec4.create(), vec4.fromValues(original[0], original[1],original[2], 1), modelViewMatrix);
+
+    // also direction?
+    //const originalD = this.position;
+    //const direction =  vec4.transformMat4(vec4.create(), vec4.fromValues(originalD[0], originalD[1],originalD[2], 1), modelViewMatrix);
+
+    this._worldPosition = position;
+
+  }
+
+  /**
+   * set the light uniforms without updating the last light position
+   */
+  setLight(context) {
+    this.setLightPosition(context);
+    this.setLightUniforms(context);
+  }
+
+  render(context) {
+    this.computeLightPosition(context);
+    this.setLight(context);
+
+    //since this a transformation node update the matrix according to my position
+    this.matrix = glm.translate(this.position[0], this.position[1], this.position[2]);
+    //render children
+    super.render(context);
+  }
 }
--- code/src/shader/phong.fs.glsl
+++ code/src/shader/phong.fs.glsl
@@ -28,11 +28,16 @@ struct Light {
 uniform Material u_material;
 uniform Light u_light;
 
+//spot
+uniform Light u_light2;
+uniform vec3 u_light2Direction; // points downward
+uniform float u_light2Angle;
 
 //varying vectors for light computation
 varying vec3 v_normalVec;
 varying vec3 v_eyeVec;
 varying vec3 v_lightVec;
+varying vec3 v_light2Vec;
 
 
 vec4 calculateSimplePointLight(Light light, Material material, vec3 lightVec, vec3 normalVec, vec3 eyeVec) {
@@ -60,9 +65,47 @@ vec4 calculateSimplePointLight(Light light, Material material, vec3 lightVec, ve
 	return c_amb + c_diff + c_spec + c_em;
 }
 
+vec4 calculateSpotLight(Light light, Material material, vec3 lightVec, vec3 normalVec, vec3 eyeVec) {
+	// You can find all built-in functions (min, max, clamp, reflect, normalize, etc.) 
+	// and variables (gl_FragCoord, gl_Position) in the OpenGL Shading Language Specification: 
+	// https://www.khronos.org/registry/OpenGL/specs/gl/GLSLangSpec.4.60.html#built-in-functions
+	lightVec = normalize(lightVec);
+	normalVec = normalize(normalVec);
+	eyeVec = normalize(eyeVec);
+
+  	//implement phong shader
+	//compute diffuse term
+	float diffuse = max(dot(normalVec,lightVec),0.0);
+
+	//compute specular term
+	vec3 reflectVec = reflect(-lightVec,normalVec);
+	float spec = pow( max( dot(reflectVec, eyeVec), 0.0) , material.shininess);
+
+	//spot ... compute angle between spot light direction and light vector
+	float angle = dot(u_light2Direction,-lightVec);
+	vec4 color;
+
+	vec4 c_amb  = clamp(light.ambient * material.ambient, 0.0, 1.0);
+
+	if (abs(angle) > u_light2Angle) {
+		vec4 c_diff = clamp(diffuse * light.diffuse * material.diffuse, 0.0, 1.0);
+		vec4 c_spec = clamp(spec * light.specular * material.specular, 0.0, 1.0);
+		vec4 c_em   = material.emission;
+
+		color = c_amb + c_diff + c_spec + c_em;
+	} else {
+		//color = c_amb;
+	}
+
+	return color;	
+}
+
 void main() {
 
 	gl_FragColor =
-		calculateSimplePointLight(u_light, u_material, v_lightVec, v_normalVec, v_eyeVec);
+		calculateSimplePointLight(u_light, u_material, v_lightVec, v_normalVec, v_eyeVec)
+		+ calculateSpotLight(u_light2, u_material, v_light2Vec, v_normalVec, v_eyeVec);
+
+	//gl_FragColor = calculateSpotLight(u_light2, u_material, v_light2Vec, v_normalVec, v_eyeVec);
 
 }
